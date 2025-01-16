ac707c3227544292a970c550d09b4774ff0581ad
Mwakamui Iluya <Mwakamui.Iluya@student.shu.ac.uk>
2025-01-16 14:51:46 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
+<?php 
+/** 
+ * Main template file for Chesterfield Connect website
+ * Sets up basic HTML structure and includes main content from index.html 
+ * Making sure DOCTYPE is consistent across CC
+ */
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>Chesterfield Connect</title>
 </head>
 <body>
-  <?php include("includes/index.html"); ?>
+  <?php 
+include("includes/index.html"); ?>
 </body>
 </html>
