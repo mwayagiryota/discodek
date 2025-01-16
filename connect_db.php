6d8ac7bc48c41b2ce4fdcc8b91e0e201d23d677b
Mwakamui Iluya <Mwakamui.Iluya@student.shu.ac.uk>
2025-01-16 14:42:36 +0000

    connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -8,11 +8,11 @@ DEFINE ('DB_NAME', 'chesconn_db1');
 // Create connection
 $dbc = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-// Check connection
+// Check connection and end script if connection failure 
 if ($dbc->connect_error) {
 	die("Connection failed: " . $dbc->connect_error);
 }
-
+// Character encoding for text handling
 $dbc -> set_charset("utf8");
 ?>
 
